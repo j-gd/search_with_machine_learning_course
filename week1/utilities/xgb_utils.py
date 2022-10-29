4d1e0fbc91cade3b72cfc15d13362782f81ce7a9
J-G Demathieu <jdemathieu@gmail.com>
2022-10-29 00:42:47 +0000

    Complete steps 3a and 4e

--- week1/utilities/xgb_utils.py
+++ week1/utilities/xgb_utils.py
@@ -33,5 +33,9 @@ def train(xgb_train_data, num_rounds=5, xgb_conf=None ):
             xgb_params = json.load(json_file)
     print("Training XG Boost on %s for %s rounds with params: %s" % (xgb_train_data, num_rounds, xgb_params))
     ##### Step 3.a
-    print("IMPLEMENT ME: train()")
+    # print("IMPLEMENT ME: train()")
+    dtrain = xgb.DMatrix(xgb_train_data)
+    num_round = 5
+    bst = xgb.train(xgb_params, dtrain, num_round)
+
     return bst, xgb_params
