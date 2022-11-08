d68c8dfed67ee9726c078cfb626322e62c5cc5f3
J-G Demathieu <jdemathieu@gmail.com>
2022-11-08 04:36:22 +0000

    Improve query.py

--- utilities/query.py
+++ utilities/query.py
@@ -9,8 +9,9 @@ import os
 from getpass import getpass
 from urllib.parse import urljoin
 import pandas as pd
-import fileinput
+# import fileinput
 import logging
+import sys
 
 
 logger = logging.getLogger(__name__)
@@ -49,7 +50,10 @@ def create_prior_queries(doc_ids, doc_id_weights,
 
 
 # Hardcoded query here.  Better to use search templates or other query config.
-def create_query(user_query, click_prior_query, filters, sort="_score", sortDir="desc", size=10, source=None):
+def create_query(user_query, click_prior_query, filters, sort="_score", sortDir="desc", size=10, source=None, synonyms=False):
+    nameField = "name"
+    if synonyms:
+        nameField = "name.synonyms"
     query_obj = {
         'size': size,
         "sort": [
@@ -65,7 +69,7 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
                         "should": [  #
                             {
                                 "match": {
-                                    "name": {
+                                    nameField: {
                                         "query": user_query,
                                         "fuzziness": "1",
                                         "prefix_length": 2,
@@ -186,11 +190,12 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
     return query_obj
 
 
-def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc"):
+def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc", synonyms=False):
     #### W3: classify the query
     #### W3: create filters and boosts
     # Note: you may also want to modify the `create_query` method above
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+
+    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"], synonyms=synonyms)
     logging.info(query_obj)
     response = client.search(query_obj, index=index)
     if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
@@ -212,6 +217,8 @@ if __name__ == "__main__":
                          help='The OpenSearch port')
     general.add_argument('--user',
                          help='The OpenSearch admin.  If this is set, the program will prompt for password too. If not set, use default of admin/admin')
+    general.add_argument('--synonyms', type=bool, default=False,
+                        help='Optional flag to also use synonyms')
 
     args = parser.parse_args()
 
@@ -224,6 +231,7 @@ if __name__ == "__main__":
     if args.user:
         password = getpass()
         auth = (args.user, password)
+    synonyms = args.synonyms
 
     base_url = "https://{}:{}/".format(host, port)
     opensearch = OpenSearch(
@@ -241,11 +249,12 @@ if __name__ == "__main__":
     index_name = args.index
     query_prompt = "\nEnter your query (type 'Exit' to exit or hit ctrl-c):"
     print(query_prompt)
-    for line in fileinput.input():
+    for line in sys.stdin:
+    # for line in fileinput.input():
         query = line.rstrip()
         if query == "Exit":
             break
-        search(client=opensearch, user_query=query, index=index_name)
+        search(client=opensearch, user_query=query, index=index_name, synonyms=synonyms)
 
         print(query_prompt)
 
