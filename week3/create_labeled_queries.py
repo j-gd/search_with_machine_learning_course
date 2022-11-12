ca28e35d414b80634cba7f7bd84c17e5bddc8ee3
J-G Demathieu <jdemathieu@gmail.com>
2022-11-12 02:45:09 +0000

    XBox has right query count

--- week3/create_labeled_queries.py
+++ week3/create_labeled_queries.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
 import csv
+import re
 
 # Useful if you want to perform stemming.
 import nltk
@@ -12,11 +13,13 @@ stemmer = nltk.stem.PorterStemmer()
 categories_file_name = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
 
 queries_file_name = r'/workspace/datasets/train.csv'
-output_file_name = r'/workspace/datasets/fasttext/labeled_queries.txt'
+output_file_name = r'/workspace/datasets/fasttext/labeled_queries_counts.txt'
+
+min_queries = 1
 
 parser = argparse.ArgumentParser(description='Process arguments.')
 general = parser.add_argument_group("general")
-general.add_argument("--min_queries", default=1,  help="The minimum number of queries per category label (default is 1)")
+general.add_argument("--min_queries", default=min_queries,  help=f"The minimum number of queries per category label (default is {min_queries})")
 general.add_argument("--output", default=output_file_name, help="the file to output to")
 
 args = parser.parse_args()
@@ -48,9 +51,25 @@ parents_df = pd.DataFrame(list(zip(categories, parents)), columns =['category', 
 queries_df = pd.read_csv(queries_file_name)[['category', 'query']]
 queries_df = queries_df[queries_df['category'].isin(categories)]
 
-# IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+# IMPLEMENT ME:
+def normalize(query):
+  # Convert all letters to lowercase.
+  low = query.lower()
+  # Treat any character that is not a number or letter as a space.
+  spaces = re.sub('[\W_]', ' ', low)
+  words = spaces.split(' ')
+  # Trim multiple spaces (which may result from previous step) to a single space.
+  noSpace = [word for word in words if len(word) > 0]
+  # Use the nltk stemmer to stem all query tokens.
+  stems = [stemmer.stem(word) for word in noSpace]
+  return ' '.join(stems)
+
+queries_df['query'] = queries_df['query'].apply(normalize)
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+all_counts = queries_df.groupby('category').size().reset_index(name='count').to_csv(output_file_name, header=False, sep='|', escapechar='\\', quoting=csv.QUOTE_NONE, index=False)
+exit()
+# enough_counts = all_counts[all_counts[0] > min_queries]
 
 # Create labels in fastText format.
 queries_df['label'] = '__label__' + queries_df['category']
---
+++ week3/level1.sh
@@ -0,0 +1,3 @@
+cat /workspace/datasets/fasttext/labeled_queries.txt | nl -w2 -s'> ' | egrep -i "Monster Pro"
+
+cat /workspace/datasets/fasttext/labeled_queries_counts.txt | egrep __label__abcat0701001
